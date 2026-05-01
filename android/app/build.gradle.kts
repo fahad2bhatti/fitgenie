cd875cafbe6089270da4bd74afbc1c7f22083911
fahad2bhatti <fahaddragon262@gmail.com>
2026-05-01 12:46:32 +0500

    feat: add delete account functionality
    # Conflicts:
    #	README.md

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.fitgenie.app"
+    namespace = "com.fahadapps.fitgenie"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -20,7 +20,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.fitgenie.app"
+        applicationId = "com.fahadapps.fitgenie"
         // Chahe to yahan direct 23 likh sakta hai:
         // minSdk = 23
         minSdk = flutter.minSdkVersion
